265914ddc73335353cfbcb63386a5cfe535d7a94
aniiketsinghh <rajputbhaishaheb@gmail.com>
2025-11-16 23:51:32 +0530

    users all functions is done

--- backend/controllers/user/user.controller.js
+++ backend/controllers/user/user.controller.js
@@ -65,10 +65,67 @@ export const Login =async(req,res)=>{
     }
 }
 
-export const GetAllUsers =(req,res)=>{}
+export const GetAllUsers =async(req,res)=>{
+    try{
+        const users= await User.find({});
+         if(!users){
+            return res.status(404).json({message:"No users found"});
+        }
+        return res.status(200).json({users});
 
-export const GetUserProfile =(req,res)=>{}
+    }catch(err){
+        console.error("Error in GetAllUsers controller:", err.message);
+    }
+}
 
-export const UpdateUserProfile =(req,res)=>{}
+export const GetUserProfile =async(req,res)=>{
+    const {userId}= req.params;
+    try{
+        const user= await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        return res.status(200).json({user});
 
-export const DeleteUserProfile =(req,res)=>{}
+      
+
+    }catch(err){
+        console.error("Error in GetUserProfile controller:", err.message);
+    }
+}
+
+export const UpdateUserProfile =async(req,res)=>{
+    const {userId}= req.params;
+    const {username, email, password}= req.body;
+    try{
+        const user= await User.findByIdAndUpdate(userId, {username, email, password});
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        return res.status(200).json({message:"User updated successfully", user});
+    }catch(err){
+        console.error("Error in UpdateUserProfile controller:", err.message);
+    }
+}
+
+export const DeleteUserProfile =(req,res)=>{
+    const {userId}= req.params;  
+    const {password}= req.body;
+    try{
+        const user= User.findById(userId);
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        if(!user.comparePassword(password)){
+            return res.status(401).json({message:"Invalid credentials"});
+        }
+        user.remove();
+        return res.status(200).json({message:"User deleted successfully"});
+
+    }
+    catch(err){
+        console.error("Error in DeleteUserProfile controller:", err.message);
+    }
+}
+
+
